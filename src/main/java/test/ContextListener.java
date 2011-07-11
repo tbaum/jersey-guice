75690a64d5e896dcb3e22c9c9f431641e07ba2f6
Stefan Hoth <sh@jnamic.com>
2011-07-11 18:40:00 +0200

    * added subpath for serving,  * added config to use Jackson-JSON instead of JAXWS

--- src/main/java/test/ContextListener.java
+++ src/main/java/test/ContextListener.java
@@ -8,28 +8,38 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
+import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ContextListener extends GuiceServletContextListener {
 
-    @Override protected Injector getInjector() {
+    @Override
+    protected Injector getInjector() {
         return Guice.createInjector(
                 new JerseyServletModule() {
-                    @Override protected void configureServlets() {
+                    @Override
+                    protected void configureServlets() {
 
                         bind(Endpoint.class);
-                        serve("/*").with(GuiceContainer.class);
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put(JSONConfiguration.FEATURE_POJO_MAPPING, "true");
+                        serve("/api/*").with(GuiceContainer.class, params);
 
                     }
                 },
                 new JpaPersistModule("unknown"),
                 new AbstractModule() {
-                    @Override protected void configure() {
+                    @Override
+                    protected void configure() {
                         bind(JpaInitializer.class).asEagerSingleton();
                     }
                 }
         );
     }
+
 }
